753a16a4fd82969a27543448855125575b0209c9
Jeffrey Aboh <jefferyaboh@gmail.com>
2025-07-09 12:26:45 +0100

    changes

--- demo/tests/index.js
+++ demo/tests/index.js
@@ -1,13 +1,18 @@
-const BlockMap_JS = require('../../index.js')({
+const blockMap = require('../../index.js')({
     API_KEY: '67ac9fd907bf8f141d7b2b71',
     EMAIL: 'developer@ethion.cloud'
 });
+// const BlockMapJS = require('@ethion/blockmap-js');
+// const blockMap = BlockMapJS({
+//     API_KEY: '67ac9fd907bf8f141d7b2b71',
+//     EMAIL: 'developer@ethion.cloud'
+// });
 
 module.exports = { }
 
 const pingServer = async () => {
     try {
-        const result = await BlockMap_JS.healthCheck();
+        const result = await blockMap.healthCheck();
         console.log(result);
     } catch (error) { console.error(error); }
 }
